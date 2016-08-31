2c430263ed65458b70b3a28ed70c2a64b465e6ba
Naimikan <guillem.aldeguer@gmail.com>
2016-08-31 13:27:03 +0200

    Added autodiscover position

--- src/factories/mapboxglUtils.js
+++ src/factories/mapboxglUtils.js
@@ -1,4 +1,4 @@
-angular.module('mapboxgl-directive').factory('mapboxglUtils', [function () {
+angular.module('mapboxgl-directive').factory('mapboxglUtils', ['$window', function ($window) {
 	/*
 		Generate Map ID by Date timestamp
 
@@ -15,18 +15,27 @@ angular.module('mapboxgl-directive').factory('mapboxglUtils', [function () {
 	*/
 	function validateAndFormatCenter (center) {
 		// [lng, lat]
-		var formattedCenter = null;
 
 		if (angular.isDefined(center)) {
-			if (angular.isNumber(center.lat) && angular.isNumber(center.lng) && (center.lng > -180 || center.lng < 180) && (center.lat > -90 || center.lat < 90)) {
-				formattedCenter = [center.lng, center.lat];
+			if (angular.isDefined(center.autodiscover) && center.autodiscover) {
+				$window.navigation.geolocation.getCurrentPosition(function (position) {
+					var coordinates = position.coords;
+
+					return [coordinates.longitude, coordinates.latitude];
+				}, function (error) {
+					console.warn('ERROR(' + error.code + '): ' + error.message);
+				}, {
+					enableHighAccuracy: true,
+  				timeout: 5000,
+  				maximumAge: 0
+				});
+			} else if (angular.isNumber(center.lat) && angular.isNumber(center.lng) && (center.lng > -180 || center.lng < 180) && (center.lat > -90 || center.lat < 90)) {
+				return [center.lng, center.lat];
 			} else if (angular.isArray(center) && center.length === 2 && angular.isNumber(center[0]) && angular.isNumber(center[1]) && (center[0] > -180 || center[0] < 180) && (center[1] > -90 || center[1] < 90)) {
-				formattedCenter = center;
+				return center;
 			} else {
 				return false;
 			}
-
-			return formattedCenter;
 		}
 
 		return false;
