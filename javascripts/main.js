c0f172fa306fcebb9e241784c741de6955a0b785
Naimikan <guillem.aldeguer@gmail.com>
2016-08-31 11:19:53 +0200

    Added mapboxgl accessToken

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,9 @@
-(function (angular) {
+(function (angular, mapboxgl, undefined) {
   'use strict';
 
-  angular.module('app', ['mapboxgl-directive']);
-})(window.angular);
+  angular.module('app', ['mapboxgl-directive'])
+
+  .run([function () {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibmFpbWlrYW4iLCJhIjoiY2lraXJkOXFjMDA0OXdhbTYzNTE0b2NtbiJ9.O64XgZQHNHcV2gwNLN2a0Q';
+  }]);
+})(window.angular, window.mapboxgl);
