c95506e1be6a0d886ae43a1722692f98819f2a7e
Naimikan <guillem.aldeguer@gmail.com>
2017-01-18 16:13:22 +0100

    Updated codeclimate.yml

--- dist/angular-mapboxgl-directive.js
+++ dist/angular-mapboxgl-directive.js
@@ -1,10 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
-*  angular-mapboxgl-directive 0.25.0 2017-01-14
+*  angular-mapboxgl-directive 0.25.0 2017-01-18
 *  An AngularJS directive for Mapbox GL
 *  git: git+https://github.com/Naimikan/angular-mapboxgl-directive.git
 */
-(function (angular, mapboxgl, undefined) {
+(function (angular, mapboxgl) {
 'use strict';
 angular.module('mapboxgl-directive', []).directive('mapboxgl', ['$q', 'mapboxglUtils', 'mapboxglConstants', 'mapboxglEventsUtils', 'mapboxglMapsData', function ($q, mapboxglUtils, mapboxglConstants, mapboxglEventsUtils, mapboxglMapsData) {
   function mapboxGlDirectiveController ($scope) {
