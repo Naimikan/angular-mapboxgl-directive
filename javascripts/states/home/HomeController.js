c5e6a496fd0a7bcdfbd7cd296a34874cb7b84db4
Guillem Aldeguer Lima <guillem.aldeguer@gmail.com>
2017-12-15 15:46:54 +0100

    Update

--- javascripts/states/home/HomeController.js
+++ javascripts/states/home/HomeController.js
@@ -4,9 +4,11 @@
   angular.module('app.Home')
 
   .controller('HomeController', ['$scope', function ($scope) {
+    $scope.glStyle = 'mapbox://styles/mapbox/dark-v9';
+
     $scope.glCenter = {
-      lat: 44.96479793033101,
-      lng: -25.2685546875
+      lat: 48.96479793033101,
+      lng: 14.2685546875
     };
 
     $scope.glZoom = {
