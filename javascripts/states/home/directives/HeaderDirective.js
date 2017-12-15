8bd7d5b601532b9f717ed67303531255d3666e92
Guillem Aldeguer Lima <guillem.aldeguer@gmail.com>
2017-12-15 13:38:23 +0100

    Update

---
+++ javascripts/states/home/directives/HeaderDirective.js
@@ -0,0 +1,29 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('app.Home')
+
+  .directive('pageHeader', ['$window', function ($window) {
+    return {
+      restrict: 'EA',
+      scope: true,
+      templateUrl: 'javascripts/states/home/directives/HeaderDirectiveTemplate.html',
+      link: function (scope, element, attrs) {
+        var header = element.find('.bg');
+        var range = 200;
+
+        $window.on('scroll', function () {
+          var scrollTop = angular.element(this).scrollTop();
+          var height = header.outerHeight();
+          var offset = height / 2;
+          var calc = 1 - (scrollTop - offset + range) / range;
+
+          header.css({ 'opacity': calc });
+
+          if (calc > '1') header.css({ 'opacity': 1 });
+          else if (calc < '0') header.css({ 'opacity': 0 })
+        });
+      }
+    };
+  }]);
+})(window.angular);
