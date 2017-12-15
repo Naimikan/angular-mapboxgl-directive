8e13d826bb9c31845803d3a7059eaa7fe71f2865
Guillem Aldeguer Lima <guillem.aldeguer@gmail.com>
2017-12-15 11:13:39 +0100

    Update

--- javascripts/states/home/HomeController.js
+++ javascripts/states/home/HomeController.js
@@ -5,7 +5,8 @@
 
   .controller('HomeController', ['$scope', function ($scope) {
     $scope.glCenter = {
-      autodiscover: true
+      lat: 78.374,
+      lng: -3.849
     };
 
     $scope.glZoom = {
@@ -14,21 +15,6 @@
 
     $scope.glHandlers = {
       scrollZoom: false
-    };
-
-    $scope.glControls = {
-      navigation: {
-        enabled: true,
-        options: {
-          position: 'top-left'
-        }
-      },
-      scale: {
-        enabled: true,
-        options: {
-          position: 'bottom-left'
-        }
-      }
     };
   }]);
 })(window.angular);
