acf59cf4bc94ed9a4c66170f6e793295ee54597b
Guillem Aldeguer Lima <guillem.aldeguer@gmail.com>
2017-12-15 13:15:42 +0100

    Update

---
+++ javascripts/states/home/directives/NavigationLinksDirective.js
@@ -0,0 +1,27 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('app.Home')
+
+  .directive('navigationLinks', ['$timeout', function ($timeout) {
+    return {
+      restrict: 'EA',
+      scope: true,
+      templateUrl: 'javascripts/states/home/directives/NavigationLinksDirectiveTemplate.html',
+      link: function (scope, element, attrs) {
+        scope.navigationLinks = [
+          {
+            href: '',
+            name: 'API Reference'
+          }, {
+            href: '',
+            name: 'Examples'
+          }, {
+            href: 'https://github.com/Naimikan/angular-mapboxgl-directive',
+            name: 'Github'
+          }
+        ];
+      }
+    };
+  }]);
+})(window.angular);
--- javascripts/states/home/directives/ProjectSelectorDirective.js
+++ javascripts/states/home/directives/ProjectSelectorDirective.js
@@ -11,19 +11,13 @@
       link: function (scope, element, attrs) {
         scope.projects = [
           {
-            href: 'https://naimikan.github.io/angular-mapboxgl-directive',
+            href: '#',
             name: 'angular-mapboxgl-directive'
           }, {
             href: 'https://naimikan.github.io/vue2-mapboxgl-component',
             name: 'vue2-mapboxgl-component'
           }
         ];
-
-        $timeout(function () {
-          element.on('hover', function (event) {
-            console.log(event);
-          });
-        }, 0);
       }
     };
   }]);
